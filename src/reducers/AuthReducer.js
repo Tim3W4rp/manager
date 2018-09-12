c28390ad3086cf8c9f74c4763a804fe54760f6c8
Martin Jordán <martin@jordan.cz>
2018-09-12 17:18:03 +0200

    add login_user_success to reducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,6 +1,7 @@
 import {
     EMAIL_CHANGED,
-    PASSWORD_CHANGED
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS
 } from '../actions/types';
 
 const initialState = {
@@ -16,6 +17,8 @@ export default (state = initialState, action) => {
             return { ...state, email: action.payload };
         case PASSWORD_CHANGED:
             return { ...state, password: action.payload };
+        case LOGIN_USER_SUCCESS:
+            return { ...state, user: action.payload };
         default:
             return state;
     }
