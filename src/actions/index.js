1455d040482c06bcf31e05feb43f8123437d513e
Martin Jordán <martin@jordan.cz>
2018-09-12 17:16:56 +0200

    refactor to helper method

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase';
 import {
     EMAIL_CHANGED,
-    PASSWORD_CHANGED
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS
 } from './types';
 
 export const emailChanged = (text) => {
@@ -18,11 +19,20 @@ export const passwordChanged = (text) => {
     };
 };
 
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
+};
+
 export const loginUser = ({ email, password }) => {
     return (dispatch) => {
         firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(user => {
-                dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+            .then(user => loginUserSuccess(dispatch, user))
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(user => loginUserSuccess(dispatch, user));
             });
     };
 };
