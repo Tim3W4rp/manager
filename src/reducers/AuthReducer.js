4665ee68a7bd72c98ecde9b4ca6a2804c69902cb
Martin Jordán <martin@jordan.cz>
2018-09-13 13:27:21 +0200

    refactoring

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,25 +1,34 @@
 import {
     EMAIL_CHANGED,
     PASSWORD_CHANGED,
-    LOGIN_USER_SUCCESS
-} from '../actions/types';
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL,
+    LOGIN_USER
+} from '../actions/types'
 
 const initialState = {
     email: '',
-    password: ''
-};
+    password: '',
+    user: null,
+    error: '',
+    loading: false
+}
 
 export default (state = initialState, action) => {
-    console.log(action);
+    console.log(action)
 
     switch (action.type) {
         case EMAIL_CHANGED:
-            return { ...state, email: action.payload };
+            return { ...state, email: action.payload }
         case PASSWORD_CHANGED:
-            return { ...state, password: action.payload };
+            return { ...state, password: action.payload }
+        case LOGIN_USER:
+            return { ...state, loading: true, error: '' }
         case LOGIN_USER_SUCCESS:
-            return { ...state, user: action.payload };
+            return { ...state, ...initialState, user: action.payload }
+        case LOGIN_USER_FAIL:
+            return { ...state, error: 'Authentication failed.', password: '', loading: false }
         default:
-            return state;
+            return state
     }
-};
+}
