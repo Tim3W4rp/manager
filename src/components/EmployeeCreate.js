cd054df904c98d7b396b5337f6e2846d4d5e9803
Martin Jordán <martin@jordan.cz>
2018-09-18 11:31:32 +0200

    refactoring

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,32 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { employeeUpdate, employeeCreate } from '../actions';
+import { Card, CardSection, Button } from './common';
+import EmployeeForm from './EmployeeForm';
 
 class EmployeeCreate extends Component {
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+
+        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+    }
+
     render() {
         return (
             <Card>
-                <CardSection>
-                    <Input
-                        label="name"
-                        placeholder="Jane"
-                        value={this.props.name}
-                        onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
-                    />
-                </CardSection>
+                <EmployeeForm {...this.props} />
 
                 <CardSection>
-                    <Input
-                        label="Phone"
-                        placeholder="555555555"
-                        value={this.props.phone}
-                        onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
-                    />
-                </CardSection>
-
-                <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Create
                     </Button>
                 </CardSection>
@@ -42,4 +33,6 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, {
+    employeeUpdate, employeeCreate
+})(EmployeeCreate);
